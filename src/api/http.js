deaa87f74aa85d48ea479b1c13a9baf452770095
dupeng <1115110893@qq.com>
2021-01-11 21:07:58 +0800

    20210111

---
+++ src/api/http.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import store from '@/store/index.js'
+axios.interceptors.response.use((ret) => {
+    if (ret.data._token) {
+        store.commit('global/setJwt',ret.data._token)
+    }
+    return ret.data || ret
+})
+axios.interceptors.request.use((cfg) => {
+    cfg.headers['authorization'] = store.state.global._token
+    return cfg
+})
+export default axios
